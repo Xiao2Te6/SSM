ed3b24c6433ca168471604a8e08036489b66403b
xiaoer <wang-xe@outlook.com>
2022-11-19 17:11:18 +0800

    演示 mybatis 的 简单增删改查

--- mybatiss_demo/src/main/java/asia/xiaoer/mybatis/mybatis_demo/mappers/UserMapper.java
+++ mybatiss_demo/src/main/java/asia/xiaoer/mybatis/mybatis_demo/mappers/UserMapper.java
@@ -1,5 +1,9 @@
 package asia.xiaoer.mybatis.mybatis_demo.mappers;
 
+import asia.xiaoer.mybatis.mybatis_demo.domain.User;
+
+import java.util.List;
+
 /**
  * @author : XiaoEr
  * @date : 2022/11/11
@@ -7,5 +11,15 @@ package asia.xiaoer.mybatis.mybatis_demo.mappers;
 public interface UserMapper {
 
     int insertUser();
+
+    int updateUser();
+
+    int deleteOne();
+
+    int deleteAll();
+
+    User selectOne();
+
+    List<User> selectAll();
 
 }
--- mybatiss_demo/src/test/java/asia/xiaoer/mybatis/mybatis_demo/mappers/UserMapperTest.java
+++ mybatiss_demo/src/test/java/asia/xiaoer/mybatis/mybatis_demo/mappers/UserMapperTest.java
@@ -1,5 +1,6 @@
 package asia.xiaoer.mybatis.mybatis_demo.mappers;
 
+import asia.xiaoer.mybatis.mybatis_demo.domain.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -8,6 +9,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -17,8 +19,10 @@ import static org.junit.Assert.*;
  */
 public class UserMapperTest {
 
+
     @Test
     public void insertUser() throws IOException {
+
         //读取MyBatis的核心配置文件
         InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
 
@@ -32,6 +36,7 @@ public class UserMapperTest {
         //创建SqlSession对象，此时通过SqlSession对象所操作的sql都会自动提交
         SqlSession sqlSession = sqlSessionFactory.openSession(true);
 
+
         //通过代理模式创建UserMapper接口的代理实现类对象(mybatis底层的动态代理)
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
 
@@ -44,5 +49,47 @@ public class UserMapperTest {
         sqlSession.close();
         is.close();
 
+    }
+
+    @Test
+    public void selectOne() throws IOException {
+        SqlSession sqlSession = getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.selectOne();
+        System.out.println(user);
+        sqlSession.close();
+    }
+
+    @Test
+    public void selectAll() {
+        SqlSession sqlSession = getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = mapper.selectAll();
+        users.forEach(System.out::println);
+        sqlSession.close();
+    }
+
+
+    //获取sqlSession方式固定可以抽取为一个工具类
+    public SqlSession getSqlSession(){
+        SqlSession sqlSession = null;
+        try {
+            //读取MyBatis的核心配置文件
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+
+            //创建SqlSessionFactoryBuilder对象
+            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+            //通过核心配置文件所对应的字节输入流创建工厂类SqlSessionFactory，生产SqlSession对象
+            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+
+            //创建SqlSession对象，此时通过SqlSession对象所操作的sql都必须手动提交或回滚事务
+            //SqlSession sqlSession = sqlSessionFactory.openSession();
+            //创建SqlSession对象，此时通过SqlSession对象所操作的sql都会自动提交
+            sqlSession = sqlSessionFactory.openSession(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sqlSession;
     }
 }
